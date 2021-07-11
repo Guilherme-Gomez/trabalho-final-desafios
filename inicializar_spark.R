4254a9807e379b3216a850e06b0b25e1f8ad8a7f
fdavi2005 <fdavi2005@gmail.com>
2021-07-11 15:19:17 -0300

    Inclusão de analise de valores faltantes e tabela de Status

--- inicializar_spark.R
+++ inicializar_spark.R
@@ -23,4 +23,33 @@ sparkR.session(master = "local[*]", sparkConfig = list(spark.driver.memory = "6g
 sqlContext <- sparkR.session(sc)
 sparkR.conf()
 
+# Basic Functions
+
+class(df)
+# Incluir coluna
+df$new_col <- df$ROW_ID
+
+# Selecionar colunas
+subsetDF <- df[,c("CODIGO_RODEIRO","CICLO_RODEIRO")]
+subsetDF
+
+# Selecionar linhas
+row_selection <- subset(df,df$ROW_ID >= 100, select = c(1:10))
+head(row_selection)
+
+# Selecionar primeira linha
+firstRow <- df[[3,]]
+firstRow
+
+# Mudar nome da coluna
+names(subsetDF) <- c("Codigo_Rodeiro","Cliclo_Rodeiro")
+
+# Funções - dim
+dim(row_selection)
+
+# Funções - head
+head(collect(count(group_by(df,"CICLO_RODEIRO"))))
+
+
+?`subset,SparkDataFrame-method`
 # Trabalho Final - Desafios e Requisitos de Projetos Analiticos
